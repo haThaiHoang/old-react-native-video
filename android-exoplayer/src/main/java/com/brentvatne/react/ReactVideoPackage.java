8b1f6ae49ff2579afbbe51d42db50afa49075ac7
Hà Thái Hoàng <hoanght@bap.jp>
2020-08-03 09:56:19 +0700

    ád

---
+++ android-exoplayer/src/main/java/com/brentvatne/react/ReactVideoPackage.java
@@ -0,0 +1,33 @@
+package com.brentvatne.react;
+
+import com.brentvatne.exoplayer.ReactExoplayerViewManager;
+import com.brentvatne.exoplayer.VideoManager;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReactVideoPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new VideoManager(reactContext));
+        return modules;
+    }
+
+    // Deprecated RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.<ViewManager>singletonList(new ReactExoplayerViewManager());
+    }
+}
